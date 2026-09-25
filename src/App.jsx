5f5a78e8d70a57eedfe29629b6ea0defb7f88871
Andres Camilo Santacruz Rojas <acsantacruzr22@gmail.com>
2026-09-24 23:27:34 -0500

    feat: integración con API real y manejo de errores

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,39 @@ import { SearchBar } from './components/SearchBar'
 import { CurrentWeatherCard } from './components/CurrentWeatherCard'
 
 function App() {
-  const [ciudad, setCiudad] = useState("Zipaquirá, Colombia")
+  const [ciudad, setCiudad] = useState("Zipaquirá")
+  const [datosClima, setDatosClima] = useState(null)
+  const [error, setError] = useState(false)
 
-  const handleBuscarCiudad = (nuevaCiudad) => {
+  const handleBuscarCiudad = async (nuevaCiudad)=> {
     setCiudad(nuevaCiudad)
+    setError(false)
+
+    const apiKey = import.meta.env.VITE_API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${nuevaCiudad}&appid=${apiKey}&units=metric&lang=es`
+
+    try {
+      const respuesta = await fetch(url)
+      const datos = await respuesta.json()
+
+      if (respuesta.ok) {
+        setDatosClima(datos)
+      } else {
+        setError(true)
+        setDatosClima(null)
+      }
+    } catch (error) {
+      console.log("Error de conexión a internet", error)
+      setError(true)
+    }
   }
 
   return (
     <div className='contenedor-movil'>
       <Header />
       <SearchBar onBuscar={handleBuscarCiudad} />
-      <CurrentWeatherCard ciudad={ciudad} />
+
+      <CurrentWeatherCard ciudad={ciudad} clima={datosClima} error={error} />
     </div>
   )
 }
--- src/components/CurrentWeatherCard.jsx
+++ src/components/CurrentWeatherCard.jsx
@@ -1,18 +1,41 @@
-export function CurrentWeatherCard({ ciudad }) {
+export function CurrentWeatherCard({ ciudad, clima, error }) {
+
+    if (error) {
+        return (
+            <div className="tarjeta-clima-principal" style={{ background: 'linear-gradient(135deg, #F59E0B, #D97706)'}}>
+                <h2 className="ciudad-titulo">Error de búsqueda</h2>
+                <p style={{ marginTop: '20px', fontSize: '1.1rem' }}>
+                    La ciudad <strong>"{ciudad}"</strong> no existe.
+                </p>
+                <p style={{marginTop: '10px', fontSize: '0.9rem', opacity: 0.9 }}>
+                    Por favor revisa la ortografía o intenta buscar por país o región cercana.
+                </p>
+            </div>
+        )
+    }
+
+    if (!clima) {
+        return (
+            <div className="tarjeta-clima-principal">
+                <h2 className="ciudad-titulo">Busca una ciudad...</h2>
+            </div>
+        )
+    }
+
     return (
         <div className="tarjeta-clima-principal">
-            <h2 className="ciudad-titulo">{ciudad}</h2>
-            <p className="hora-local">Hora local: 15:30</p>
-
+            <h2 className="ciudad-titulo">{clima?.name || ciudad}, {clima?.sys?.country || ""}</h2>
             <div className="temperatura-gigante">
-                <h1>19°C</h1>
+                <h1>{clima?.main?.temp ? Math.round(clima.main.temp) : "--"}°C</h1>
             </div>
 
-            <p className="condicion-clima">Soleado</p>
+            <p className="condicion-clima" style= {{ textTransform: 'capitalize' }}>
+                {clima?.weather?.[0]?.description || "Sin información"}
+            </p>
 
             <div className="detalles-extra">
-                <span>Humedad: 20%</span>
-                <span>Viento: 14 km/h</span>
+                <span>Humedad: {clima?.main?.humidity || "--"}%</span>
+                <span>Viento: {clima?.wind?.speed || "--"} m/s</span>
             </div>
         </div>
     )
